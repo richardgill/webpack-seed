adbfa5e30f38a45592e6a0bf2749891ae264e999
Richard Gill <richard@rgill.co.uk>
2015-10-22 22:37:04 +0100

    Babel working scss added

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+console.log('Hello World')
+
+var helloWorld1 = () => 'Hello World1'
+
+
+var stringWithVar = `${helloWorld1()} 1234`
+console.log(stringWithVar)
+
+throw 'Oh no!'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/main.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+  entry: [
+    'webpack/hot/dev-server', //Hot reload
+    'webpack-dev-server/client?http://localhost:8080', //Refresh when src changes
+    path.resolve(__dirname, 'src/scripts/main.js')
+  ],
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
     },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }]
+  }
+
 };
